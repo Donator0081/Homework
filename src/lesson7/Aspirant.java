29bf98183d61248f4a675574993dcc87af476677
t-vieru <t_vieru@hotmail.com>
2018-01-10 21:24:29 +0200

    Homework

---
+++ src/lesson7/Aspirant.java
@@ -0,0 +1,38 @@
+package lesson7;
+
+/**
+ * Created by student on 1/10/2018.
+ */
+public class Aspirant extends Student {
+    private String scienceWork;
+
+    public Aspirant(String firstName, String lastName, String group, double averageMark, String scienceWork) {
+        super(firstName, lastName, group, averageMark);
+        this.scienceWork = scienceWork;
+    }
+
+    public String getScienceWork() {
+        return scienceWork;
+    }
+
+    public void setScienceWork(String scienceWork) {
+        this.scienceWork = scienceWork;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("Илья","Иванов","BA",5);
+        Aspirant aspirant1 = new Aspirant("Илья1","Иванов1","BA1",4,"Научная работа Ф");
+        Student aspirant2 = new Aspirant("Илья2","Иванов2","BA2",3,"Научная работа Ф");
+        System.out.println(aspirant1.getFirstName());
+        System.out.println(aspirant1.getLastName());
+        System.out.println(aspirant1.getGroup());
+        System.out.println(aspirant1.getAverageMark());
+        System.out.println(aspirant1.getScienceWork());
+
+        System.out.println(aspirant2.getFirstName());
+        System.out.println(aspirant2.getLastName());
+        System.out.println(aspirant2.getGroup());
+        System.out.println(aspirant2.getAverageMark());
+        //System.out.println(aspirant2.getScienceWork());
+    }
+}
---
+++ src/lesson7/Student.java
@@ -0,0 +1,48 @@
+package lesson7;
+
+/**
+ * Created by student on 1/10/2018.
+ */
+public class Student {
+    private String firstName,lastName,group;
+    double averageMark;
+
+    public Student(String firstName, String lastName, String group, double averageMark) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.group = group;
+        this.averageMark = averageMark;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public double getAverageMark() {
+        return averageMark;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public void setAverageMark(double averageMark) {
+        this.averageMark = averageMark;
+    }
+}
