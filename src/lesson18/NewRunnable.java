baf9b934c0fa3e947bfc6748fbaea3ca3bfd8a1c
Donator0081 <iliya00817@gmail.com>
2018-02-28 21:11:14 +0200

    Homework

--- src/lesson18/NewRunnable.java
+++ src/lesson18/NewRunnable.java
@@ -10,6 +10,11 @@ public class NewRunnable implements Runnable {
             if (i % 10 == 0) {
                 System.out.println(i);
             }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                System.out.println(e.getMessage());
+            }
         }
     }
 }
