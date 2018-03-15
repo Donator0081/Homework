77d171e7257c3ad8fbeffe0adb7eecf4a4571be9
Donator0081 <iliya00817@gmail.com>
2018-03-15 20:11:07 +0200

    Homework

--- src/lesson12/AverageString.java
+++ src/lesson12/AverageString.java
@@ -2,11 +2,15 @@ package lesson12;
 
 public class AverageString {
     public static void main(String[] args) {
-        StringBuilder sb1 = new StringBuilder("string");
-        StringBuilder sb2 = new StringBuilder("code");
-        StringBuilder sb3 = new StringBuilder("Practice");
-        System.out.println(sb1.substring(2, 4));
-        System.out.println(sb2.substring(1, 3));
-        System.out.println(sb3.substring(3, 5));
+        String[] str = {"toy", "clouds", "horny"};
+
+        for (String string : str) {
+            System.out.println(findAverString(string));
+        }
+    }
+
+    public static String findAverString(String str) {
+        int length = str.length();
+        return str.substring(length / 2 - 1, length / 2 + 1);
     }
 }
